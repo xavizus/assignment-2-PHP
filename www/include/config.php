3f30b8326a4119422185c222d7ce008629ef5a35
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-01-25 18:47:11 +0100

    Removed copy of same information from config.ini.

--- www/include/config.php
+++ www/include/config.php
@@ -1,12 +1,6 @@
 <?php 
-  define('DB_HOST', 'xav-p-mariadb01.xavizus.com');
-  define('DB_USER', 'assignment2-php');
-  define('DB_PASS', 'ANm2eAoXoxUAtDeN');
-  define('DB_NAME', 'assignment2-php');
-  define('DB_PORT', 16200);
-
   // URL Root
-define('URLROOT', '');
+define('URLROOT', '/assignment-2-PHP/www');
   // Site Name
 define('SITENAME', 'Grupp_H_PHP');
 
