2d6b1ab1561d7ebeb4f98a1ee9a9e02756372ac1
Stephan Ljungros <stephan.ljungros@gmail.com>
2020-01-28 15:59:08 +0100

    Fixed header error

--- www/index.php
+++ www/index.php
@@ -15,22 +15,24 @@ spl_autoload_register('autoload');
     <div class="col-md-10 mx-auto">
       <div class="card card-body bg-light mt-5">
 
-        <h2>Login elller vaddddd</h2>
+        <h2>Login</h2>
         <p>Please fill in your credentials to log in</p>
         <form action="<?php echo URLROOT; ?>/user/login.php" method="post">
           <div class="form-group">
             <label for="email">Email: <sup>*</sup></label>
-            <input type="email" name="email" class="form-control form-control-lg ">
+            <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($_GET['msg_err'])) ? 'is-invalid' : ''; ?>">
+            <span class="invalid-feedback"><?php echo $_GET['msg_err']; ?></span>
 
           </div>
           <div class="form-group">
             <label for="password">Password: <sup>*</sup></label>
-            <input type="password" name="password" class="form-control form-control-lg" >
+            <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($_GET['msg_err'])) ? 'is-invalid' : ''; ?>" >
+            <span class="invalid-feedback"><?php echo $_GET['msg_err']; ?></span>
 
           </div>
           <div class="row">
             <div class="col">
-              <input type="submit" value="Login" class="btn btn-success btn-block">
+              <input type="submit" name="submit" value="Login" class="btn btn-success btn-block">
             </div>
             <div class="col">
               <a href="<?php echo URLROOT; ?>/user/register.php" class="btn btn-light btn-block">No account? Register</a>
--- www/user/login.php
+++ www/user/login.php
@@ -1,5 +1,3 @@
-
-
 <?php
 
 include '../include/header.php';
@@ -7,14 +5,6 @@ require '../classes/settings.class.php';
 include '../classes/Database.class.php';
 include '../classes/User.php';
 
-$data = [
-    'email' => '',
-    'password' => '',
-    'email_err' => '',
-    'password_err' => '',
-];
-
-if (isset($_POST['submit'])) {
     // Init data
     $data = [
         'email' => trim($_POST['email']),
@@ -26,19 +16,11 @@ if (isset($_POST['submit'])) {
     if (empty($data['email'])) {
         $data['email_err'] = 'Please enter email';
     } 
-    // elseif ($data['email'] !== $data['email']) {
-    //     //email not found
-    //     $data['email_err'] = 'Email not found';
-    // }
-        
+
     // Validate Password
     if (empty($data['password'])) {
         $data['password_err'] = 'Please enter password';
     }
-    //  elseif ($data['password'] !== $data['password']) {
-    //     //funkar ej
-    //     $data['password_err'] = 'Password not found';
-    // }
 
     if (empty($data['email_err']) && empty($data['password_err'])) {
         // $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
@@ -50,46 +32,8 @@ if (isset($_POST['submit'])) {
         {
             header('Location: /success.php');
         }
-        if ($data['email'] !== $data['email' ]);
-        {
-            $data['email_err'] = 'Email not found';
+        else {
+          header('Location: /index.php?msg_err=Det gick inte att logga in');
         }
-        if ($data['password'] !== $data['password' ]);
-        {
-            $data['password_err'] = 'Password not found';
-        }
+        
     }
-}
-?>
-
-<div class="row pt-4 pb-4">
-    <div class="col-md-10 mx-auto">
-      <div class="card card-body bg-light mt-5">
-
-        <h2>Login</h2>
-        <p>Please fill in your credentials to log in</p>
-        <form action="<?php echo URLROOT; ?>/user/login.php" method="post">
-          <div class="form-group">
-            <label for="email">Email: <sup>*</sup></label>
-            <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>">
-            <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
-
-          </div>
-          <div class="form-group">
-            <label for="password">Password: <sup>*</sup></label>
-            <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" >
-            <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
-
-          </div>
-          <div class="row">
-            <div class="col">
-              <input type="submit" name="submit" value="Login" class="btn btn-success btn-block">
-            </div>
-            <div class="col">
-              <a href="<?php echo URLROOT; ?>/user/register.php" class="btn btn-light btn-block">No account? Register</a>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
-  </div>
