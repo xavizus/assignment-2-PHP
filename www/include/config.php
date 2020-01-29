be0360e47a487e316ce5d93be7b2374c918bebd9
yde <philip.ydehed@gmail.com>
2020-01-29 11:04:35 +0100

    fixed from //user/login.php to /user/login.php

--- www/include/config.php
+++ www/include/config.php
@@ -3,6 +3,6 @@ if (!defined('ROOT_DIR')) {
     define("ROOT_DIR", __DIR__ . "/../../");
 }
 // URL Root
-define('URLROOT', (!empty($_SERVER['HTTPS']) ? 'https' : 'http') ."://". $_SERVER['HTTP_HOST'].'/');
+define('URLROOT', (!empty($_SERVER['HTTPS']) ? 'https' : 'http') ."://". $_SERVER['HTTP_HOST'].'');
 // Site Name
 define('SITENAME', 'Grupp_H_PHP');
