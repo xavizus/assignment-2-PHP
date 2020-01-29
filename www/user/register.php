3cdcbc29cadd71dcc38b5bf0f784ed8e23a96047
yde <philip.ydehed@gmail.com>
2020-01-29 13:10:30 +0100

    adding header to redirect to login when you have successfully registrated a user

--- www/user/register.php
+++ www/user/register.php
@@ -61,6 +61,7 @@ if (isset($_POST['submit'])) {
         $database = new Database(new \Settings());
         $user = new classes\User($database);
         $user->register($data);
+        header('Location: /');
     }
 }
 
